716a4a461494a0406cb13c490e6489cc101e0478
Lex <lex.zhao@chicagobooth.edu>
2015-11-23 16:06:31 -0600

    Fixed all user list show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @photos = Photo.where(:user_id => current_user.id)
+    @photos = Photo.where(:user_id => params[:id])
     @comments = Comment.all
     @likes = Like.all
   end
